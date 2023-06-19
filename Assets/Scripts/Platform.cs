c397b7d77734ce6b0dfff67290960eed54dd9aca
Davi Gonçalves Valério <al.davigvalerio@gmail.com>
2023-06-19 13:21:11 -0300

    feat(platform): spawn platform infinite

---
+++ Assets/Scripts/Platform.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    public Transform endPoint;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/SpawnPlatform.cs
+++ Assets/Scripts/SpawnPlatform.cs
@@ -5,25 +5,42 @@ using UnityEngine;
 public class SpawnPlatform : MonoBehaviour
 {
     public List<GameObject> platforms = new List<GameObject>();
+    public List<Transform> currentPlatforms = new List<Transform>();
     public int platformDistance = 0;
+
+    private Transform _player;
+    private Transform _currentPlatformPoint;
+    private int _currentPlatformIndex;
 
     private void Start()
     {
+        _player = GameObject.FindGameObjectWithTag("Player").transform;
+        
         foreach (var platform in platforms)
         {
-            Instantiate(platform, new Vector3(0,0,platformDistance), transform.rotation);
+            var updatedPlatform = Instantiate(platform, new Vector3(0,0,platformDistance), transform.rotation).transform;
+            currentPlatforms.Add(updatedPlatform);
             platformDistance += 87;
         }
+
+        _currentPlatformPoint = currentPlatforms[_currentPlatformIndex].GetComponent<Platform>().endPoint;
     }
 
-    public GameObject myPlat;
-    
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A))
+        var distance = _player.position.z - _currentPlatformPoint.position.z;
+
+        if (!(distance >= 5)) return;
+
+        Recycle(currentPlatforms[_currentPlatformIndex].gameObject);
+        _currentPlatformIndex++;
+
+        if (_currentPlatformIndex > currentPlatforms.Count - 1)
         {
-            Recycle(myPlat);
+            _currentPlatformIndex = 0;
         }
+
+        _currentPlatformPoint = currentPlatforms[_currentPlatformIndex].GetComponent<Platform>().endPoint;
     }
 
     public void Recycle(GameObject platform)
